2f347283d367f78ebdfc245135100213a241b694
Simon Lee <simon@250ok.com>
2018-05-07 15:27:17 -0400

    Ready to begin Section 10 Redux

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,6 @@ const Header = () => (
 		<h1>The Expensify App</h1>
 		<NavLink to='/' activeClassName="is-active" exact={true}>Home</NavLink>
 		<NavLink to='/create' activeClassName="is-active">Create New</NavLink>
-		<NavLink to='/edit' activeClassName="is-active">Edit</NavLink>
 		<NavLink to='/help' activeClassName="is-active">Help</NavLink>
 	</header>
 );
